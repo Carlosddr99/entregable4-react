26fdb2c7a8fdefce5586468689ad98a49c75070d
Carlos Daniel <carlosddr1999@hotmail.com>
2023-07-09 19:02:26 +0100

    Toda la aplicacion

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import ProductsPage from './productPage/ProductsPage.js';
+
+function App() {
+  return (
+    <div className="App">
+      <ProductsPage />
+    </div>
+  );
+}
+
+export default App;
